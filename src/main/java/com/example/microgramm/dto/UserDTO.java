ab5331ccaf12b0c3955fc2b453394fbec720c6c9
IgorPriadein <iga2456@gmail.com>
2022-03-29 17:09:05 +0600

    Lesson 53 - Finished UserDTO

--- src/main/java/com/example/microgramm/dto/UserDTO.java
+++ src/main/java/com/example/microgramm/dto/UserDTO.java
@@ -1,4 +1,35 @@
 package com.example.microgramm.dto;
 
+
+import com.example.microgramm.entity.User;
+import lombok.*;
+
+import javax.persistence.Column;
+
+@Data
+@Builder(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class UserDTO {
+
+    public static UserDTO from(User user){
+
+
+        return builder()
+                .id(user.getId())
+                .name(user.getName())
+                .email(user.getEmail())
+                .countPublications(user.getCountPublications())
+                .countSubscribers(user.getCountSubscribes())
+                .countSubscribes(user.getCountSubscribes())
+                .build();
+    }
+
+    private Long id;
+    private String name;
+    private String email;
+    private Integer countPublications;
+    private Integer countSubscribers;
+    private Integer countSubscribes;
+
 }
